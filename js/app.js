a7686c355d1f52bc36c519e86d9a2a2c43affac0
amanske <alexanderjanmanske@gmail.com>
2017-02-04 19:04:53 +0100

    fixed bug in app.js

--- js/app.js
+++ js/app.js
@@ -6,7 +6,9 @@ $(function() {
 	//And create the needed controllers and views
     var topBanner = new TopBanner($("#topBanner1"));
     var startMessage = new StartMessage($("#mainStart"));
-    var itemDescription = new ItemDescription($("#page3"), model);
+    if ($("#page3").length){ //if div exists
+        var itemDescription = new ItemDescription($("#page3"), model);
+    }
     var sideBar = new SideBar($("#sideBarDiv"));
 
 });
